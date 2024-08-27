e445e062d0c53899b1e6b3a75a9af77016029c75
Dashon <i@dashon.co>
2024-08-26 22:46:32 -0500

    chore: remove unused import from main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from app.hello import hello
 from app.routes import translation, vocabulary
 
 app = FastAPI()
 
 app.include_router(translation.router, prefix="/translate")
 app.include_router(vocabulary.router, prefix="/vocab")
+
 
 @app.get("/")
 def read_root():
